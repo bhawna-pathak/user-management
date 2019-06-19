183e5d57eb16194205996fff326f095256755daa
bhawna-pathak <bhawna9123@gmail.com>
2019-06-20 00:28:37 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import './App.css';
-// import UserList from '../src/Components/UserList';
-import Register from '../src/Components/Register';
-// import Login from '../src/Components/Login';
+import React from "react";
+import "./App.css";
+import UserList from "../src/Components/UserList";
+import Register from "../src/Components/Register";
+import Login from "../src/Components/Login";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <UserList/> */}
-        <Register/>
-        {/* <Login/> */}
+        <Router>
+          <Route path="/" exact component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/users" component={UserList} />
+        </Router>
       </header>
     </div>
   );
